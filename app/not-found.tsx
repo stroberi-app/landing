d2e5ddb0d7d2b651084583796b9c36bdff62833c
Danilo Gacevic <gacevic.danilo@gmail.com>
2025-01-03 14:52:53 +0100

    custom not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center flex-col gap-4">
+      <h1 className="text-2xl font-bold">Woops! Seems like you missed it</h1>
+      <a  href="/" className={buttonStyles}>Press here to go back
+      </a>
+    </div>
+  )
+}
+
+const buttonStyles = "bg-brand text-white px-4 py-2 rounded-md"
